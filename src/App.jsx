36bf5667fa7be224a3fcd5fa5c8c6ddd543657c2
gonzalo <gonzalosuarez@frba.utn.edu.ar>
2024-11-15 16:51:06 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,30 @@ import Stack from "./components/organisms/Stack";
 import Footer from "./components/organisms/Footer";
 import { ScrollContextProvider } from "./contexts/ScrollContext";
 import ToTopArrow from "./components/atoms/ToTopArrow";
+import { useEffect } from "react";
+import axios from "axios";
 import "./styles/app.css";
 
 function App() {
+  useEffect(() => {
+    axios.get("https://api.ipify.org?format=json").then((response) => {
+      const visitorIp = response.data.ip;
+      const myIp = "186.22.245.51";
+
+      if (visitorIp !== myIp) {
+        const mensaje = {
+          content: `Un visitante con la IP ${visitorIp} ha accedido a tu portfolio web.`,
+          username: "Alerta de Acceso",
+        };
+
+        const webhookUrl =
+          "https://discord.com/api/webhooks/1223315522725351434/yHQuT0etT59ZpTMEQtHUVWko9XF-TPRxXmMOznK0GBigTiUkHpzUWjCjPZjToDbYZC3B";
+
+        axios.post(webhookUrl, mensaje);
+      }
+    });
+  }, []);
+
   return (
     <ThemeContextProvider>
       <ScrollContextProvider>
