98b5d1f31a68275c5aa20065f4f8453a0a44a66a
gonzalo <gonzalosuarez@frba.utn.edu.ar>
2026-01-27 14:14:31 -0300

    Fix Nav Hover

--- src/components/atoms/NavMobileItem.jsx
+++ src/components/atoms/NavMobileItem.jsx
@@ -6,7 +6,7 @@ export default function NavMobileItem({ name, onClick, isActive }) {
   return (
     <div onClick={onClick}>
       <p
-        style={{ backgroundColor: isActive && "#4070f4" }}
+        style={{ backgroundColor: isActive && "#4070f4", color: isActive && "#ffffff" }}
         className={`navitem_${theme} text_${theme} language p-3 m-0`}
       >
         {name}
